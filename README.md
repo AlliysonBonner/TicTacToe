f59130487885d55d58f71d3c867e42bb46d49461
AlliysonBonner <alliyson.bonner@gmail.com>
2023-02-17 01:46:35 -0600

    Optimize game.py and player.py, add image and text to README.md, and add LICENSE.md  - Refactor code to improve efficiency and readability - Add image.png and description of project to README.md - Include licensing information in LICENSE.md

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# TicTacToe
-TicTacToe written in Python
+# Tic Tac Toe Game
+
+![Screenshot of Tic Tac Toe game being played with X and O in the boxes](https://github.com/AlliysonBonner/TicTacToe/blob/main/image.png?raw=true)
+
+This is a simple implementation of the classic Tic Tac Toe game in Python.
+
+## How to Play
+
+1. Clone this repository to your local machine.
+2. Open a terminal and navigate to the root directory of the repository.
+3. Run the following command to start the game:
+
+    ```
+    python game.py
+    ```
+
+4. Choose whether you want to play as "X" or "O" by entering your choice at the prompt.
+5. Enter your moves at the prompt by typing the number corresponding to the square you want to choose.
+6. The game will continue until someone wins or the board is full.
+7. Play again by running `python game.py` again.
+
+## About the Code
+
+The game logic is implemented in the `game.py` file, and the players are implemented in the `player.py` file. The `HumanPlayer` class allows a human player to enter moves at the prompt, while the `RandomComputerPlayer` class makes random moves.
+
+## Requirements
+
+This game requires Python 3.7 or higher to run. There are no additional dependencies.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE.md](https://github.com/alliysonbonner/TicTacToe/LICENSE.md) file for details.
+
+## Author
+* Name: Alliyson Bonner
+* Github: https://github.com/alliysonbonner
+* LinkedIn: https://www.linkedin.com/in/alliyson
+* Email: alliyson.bonner@gmail.com
--- player.py
+++ player.py
@@ -1,43 +1,112 @@
-import math
 import random
 
+
 class Player:
+    """Represents a player in the game.
+
+    Attributes:
+        letter (str): A string representing the player's letter ('X' or 'O').
+
+    Methods:
+        get_move(game): Get the player's next move given the current game state.
+    """
+
     def __init__(self, letter):
-        # letter is x or o
+        """Initializes a new instance of the Player class.
+
+        Args:
+            letter (str): A string representing the player's letter ('X' or 'O').
+        """
         self.letter = letter
 
-    # we want all players to get thei next move given a game
     def get_move(self, game):
+        """Get the player's next move given the current game state.
+
+        This method must be implemented by all subclasses of the Player class.
+
+        Args:
+            game (TicTacToe): The current TicTacToe game instance.
+
+        Returns:
+            int: The index of the square the player has chosen to mark on the board.
+        """
         pass
 
+
 class RandomComputerPlayer(Player):
+    """Represents a computer player that makes random moves.
+
+    Inherits from the Player class.
+
+    Attributes:
+        letter (str): A string representing the player's letter ('X' or 'O').
+
+    Methods:
+        get_move(game): Get the computer player's next move given the current game state.
+    """
+
     def __init__(self, letter):
+        """Initializes a new instance of the RandomComputerPlayer class.
+
+        Args:
+            letter (str): A string representing the player's letter ('X' or 'O').
+        """
         super().__init__(letter)
 
     def get_move(self, game):
+        """Get the computer player's next move given the current game state.
+
+        Args:
+            game (TicTacToe): The current TicTacToe game instance.
+
+        Returns:
+            int: The index of a randomly chosen square that is available on the board.
+        """
         square = random.choice(game.available_moves())
-        #get a random valid spot for our next move
         return square
 
+
 class HumanPlayer(Player):
+    """Represents a human player that gets moves from user input.
+
+    Inherits from the Player class.
+
+    Attributes:
+        letter (str): A string representing the player's letter ('X' or 'O').
+
+    Methods:
+        get_move(game): Get the human player's next move given the current game state.
+    """
+
     def __init__(self, letter):
+        """Initializes a new instance of the HumanPlayer class.
+
+        Args:
+            letter (str): A string representing the player's letter ('X' or 'O').
+        """
         super().__init__(letter)
 
     def get_move(self, game):
+        """Get the human player's next move given the current game state.
+
+        Prompts the user to input a square to mark on the board, validates the input,
+        and returns the index of the chosen square.
+
+        Args:
+            game (TicTacToe): The current TicTacToe game instance.
+
+        Returns:
+            int: The index of the square the user has chosen to mark on the board.
+        """
         valid_square = False
         val = None
         while not valid_square:
-            square = input(self.letter + '\'s turn. Input move (0-9):')
-            # we're going to check that this is a correct value by trying to
-            # cast it to an integer, and if it's not, then we say it's valid
-            # if that spot is not available on the board, we also say its
-            # invalid
+            square = input(self.letter + "'s turn. Input move (0-8): ")
             try:
                 val = int(square)
                 if val not in game.available_moves():
                     raise ValueError
-                valid_square = True # if these are successful, then yay!
+                valid_square = True
             except ValueError:
-                print('Invalid squsre. Try again')
-
+                print("Invalid square. Try again.")
         return val
